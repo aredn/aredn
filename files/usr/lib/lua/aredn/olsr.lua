918599a6f151a85f8af60c3d829769dee6657f4a
ab7pa <ab7pa.radio@gmail.com>
2021-05-20 12:43:38 -0700

    Make hostname lowercase for consistency with Local Hosts display in newui

--- files/usr/lib/lua/aredn/olsr.lua
+++ files/usr/lib/lua/aredn/olsr.lua
@@ -76,7 +76,7 @@ function model.getCurrentNeighbors(RFinfo)
 	  if host ~= nil then
     	host = string.gsub(host,"mid%d.", "")
       host = string.gsub(host,".local.mesh$","")
-      host = string.upper(host)
+      host = string.lower(host)
     	info[remip]['hostname']=host
 	  else
 		  info[remip]['hostname']=remip
