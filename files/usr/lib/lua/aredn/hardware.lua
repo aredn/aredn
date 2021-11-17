fe4da9a9f6ccf6257a78cee2f741fcef32fb3cc1
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-11-17 11:14:03 -0800

    Start a hardware library for all the hardware specific info only

---
+++ files/usr/lib/lua/aredn/hardware.lua
@@ -0,0 +1,70 @@
+--[[
+
+  Part of AREDN -- Used for creating Amateur Radio Emergency Data Networks
+  Copyright (C) 2021 Tim Wilkinson
+  See Contributors file for additional contributors
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation version 3 of the License.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  Additional Terms:
+
+  Additional use restrictions exist on the AREDN(TM) trademark and logo.
+    See AREDNLicense.txt for more info.
+
+  Attributions to the AREDN Project must be retained in the source code.
+  If importing this code into a new or existing project attribution
+  to the AREDN project must be added to the source code.
+
+  You must not misrepresent the origin of the material contained within.
+
+  Modified versions must be modified to attribute to the original source
+  and be marked in reasonable ways as differentiate it from the original
+  version.
+
+--]]
+
+local json = require("luci.jsonc")
+
+local hardware = {}
+
+local radio_json = nil
+function get_radio_json()
+    if not radio_json then
+        local f = io.open("/etc/radios.json")
+        if not f then
+            return {}
+        end
+        radio_json = json.parse(f:read("*a"))
+        f:close()
+    end
+    return radio_json
+end
+
+function hardware.wifi_maxpower(channel)
+    local board = get_radio_json()[name]
+    if board then
+        if board.chanpower then
+            for k, v in pairs(board.chanpower)
+            do
+                if channel <= tonumber(k) then
+                    return tonumber(v)
+                end
+            end
+        elseif board.maxpower then
+            return tonumber(board.maxpower)
+        end
+    end
+    return 27 -- if all else fails
+end
+
+return hardware
