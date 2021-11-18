15e4f7552bd08756b6d1914b43e5efd01a6101f4
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-11-17 17:43:14 -0800

    Use new link led api

--- files/usr/local/bin/mgr/linkled.lua
+++ files/usr/local/bin/mgr/linkled.lua
@@ -5,16 +5,16 @@ function linkled()
     local link = hardware.get_link_led()
     
     -- Reset leds
-    utils.write_all(link1led .. "/trigger", "none")
-    utils.write_all(link1led .. "/brightness", "0")
+    utils.write_all(link .. "/trigger", "none")
+    utils.write_all(link .. "/brightness", "0")
 
     while true
     do
         local nei = utils.fetch_json("http://127.0.0.1:9090/neighbors")
         if nei and #nei.neighbors > 0 then
-            utils.write_all(link1led .. "/brightness", "1")
+            utils.write_all(link .. "/brightness", "1")
         else
-            utils.write_all(link1led .. "/brightness", "0")
+            utils.write_all(link .. "/brightness", "0")
         end
         wait_for_ticks(11)
     end
