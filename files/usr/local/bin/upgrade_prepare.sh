71938995c793c63e61db4967dc7d11392a99e628
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2022-04-20 23:48:13 -0700

    Run the iperf3 server by default

--- files/usr/local/bin/upgrade_prepare.sh
+++ files/usr/local/bin/upgrade_prepare.sh
@@ -37,7 +37,7 @@ LICENSE
 # ServiceName[:ServiceDaemon] pairs.
 # If ServiceDaemon is omitted, we wont first kill the daemon
 #
-SERVICES="dnsmasq:dnsmasq dropbear:dropbear urngd:urngd rpcd:rpcd telnet:telnetd manager:manager.lua log:logd"
+SERVICES="dnsmasq:dnsmasq dropbear:dropbear urngd:urngd rpcd:rpcd telnet:telnetd manager:manager.lua log:logd iperf3:iperf4"
 
 #
 # We unceremoniously kill services, and then stop them to prevent
