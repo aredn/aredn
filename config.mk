55869611868d80447b36cb57f4a670ef595ba140
Joe AE6XE <ae6xe@arrl.net>
2022-09-24 10:27:22 -0700

    aredn:  update mesh-release and revert config.mk

--- config.mk
+++ config.mk
@@ -1,13 +1,13 @@
 # default parameters for Makefile
 
 # What version will show in the AREDN firmware 'Node Status" UI page?
-PRIVATE_BUILD_VERSION=AE6XE
+PRIVATE_BUILD_VERSION=NoCall
 
 # build options:  -j# for (# of cores +1) on build machine,  V=s for verbose output
 # https://wiki.openwrt.org/doc/howto/build#make_options  (archive)
 # https://openwrt.org/docs/guide-developer/usebuildsytem (openwrt-lede merge)
 # example "MAKE_ARGS=-j9 V=s IGNORE_ERRORS=m BUILD_LOG=1"
-MAKE_ARGS=-j5
+MAKE_ARGS=-j3
 
 # Where will the installed image find add-on Packages to download?
 # This URL must contain the packages from this build
