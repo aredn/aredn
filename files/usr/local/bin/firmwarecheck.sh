928d85d4b01814d980b052cf9fa6e8a14c7a29f5
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2023-01-05 16:37:39 -0800

    Provide better error feedback when upgrades fail

--- files/usr/local/bin/firmwarecheck.sh
+++ files/usr/local/bin/firmwarecheck.sh
@@ -5,11 +5,20 @@
 
 include /lib/upgrade/
 
-if (eval platform_check_image "$*")
+if $(platform_check_image "$*" > /dev/null 2>&1)
 then
-	return 0;
-else
-	return 1;
+	json=$(/usr/libexec/validate_firmware_image "$*" 2> /dev/null)
+	if [ "$(echo "$json" | jsonfilter -e '@.value')" = "true" ]; then
+		return 0;
+	fi
+	if [ "$(echo "$json" | jsonfilter -e '@.tests.fwtool_signature')" = "false" ]; then
+		echo "firmware signature failed"
+		return 1;
+	fi
+	if [ "$(echo "$json" | jsonfilter -e '@.tests.fwtool_device_match')" = "false" ]; then
+		echo "firmware device match failed";
+		return 1
+	fi
 fi
-
-
+echo "platform check image failed";
+return 1
