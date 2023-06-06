da7b894c3988266d1313953767ac8fe286f0cc1c
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2023-06-06 00:38:45 -0700

    Support xlinks on x86 (#865)

--- files/usr/lib/lua/aredn/nav/admin/65advancednetwork.lua
+++ files/usr/lib/lua/aredn/nav/admin/65advancednetwork.lua
@@ -1,4 +1,4 @@
 local board = aredn.hardware.get_board_type()
-if board == "mikrotik,hap-ac2" or board == "mikrotik,hap-ac3" then
+if board == "mikrotik,hap-ac2" or board == "mikrotik,hap-ac3" or board == "qemu-standard-pc-i440fx-piix-1996" then
     return { href = "advancednetwork", display = "Advanced Network" }
 end
