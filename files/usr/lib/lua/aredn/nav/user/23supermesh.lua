2483ce938cfb42aa5f9186e19988eccf33c95fce
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2023-09-06 11:05:40 -0700

    Improve supernode detection So it doesnt keep writing to flash

--- files/usr/lib/lua/aredn/nav/user/23supermesh.lua
+++ files/usr/lib/lua/aredn/nav/user/23supermesh.lua
@@ -1,5 +1,5 @@
 if nixio.fs.stat("/tmp/supernode.dns") then
-    local ip = read_all("/tmp/supernode.dns"):match("^(%S+)")
+    local ip = read_all("/tmp/supernode.dns"):match("^#(%S+)")
     if ip then
         return { href = "http://" .. ip .. "/cgi-bin/mesh", display = "Super Mesh", hint = "See what is on the supernode mesh" }
     end
