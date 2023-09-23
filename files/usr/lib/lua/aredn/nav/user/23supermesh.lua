e35887dff368d462dc50642c86826b84c9b421fb
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2023-09-22 23:59:46 -0700

    Supernode tunnels are always "perfect"

--- files/usr/lib/lua/aredn/nav/user/23supermesh.lua
+++ files/usr/lib/lua/aredn/nav/user/23supermesh.lua
@@ -1,8 +1,6 @@
 if nixio.fs.stat("/tmp/dnsmasq.d/supernode.conf") then
     local ip = read_all("/tmp/dnsmasq.d/supernode.conf"):match("^#(%S+)")
     if ip then
-        return { href = "http://" .. ip .. "/cgi-bin/mesh", display = "Super Mesh", hint = "See what is on the supernode mesh" }
+        return { href = "http://" .. ip .. "/cgi-bin/mesh", display = "Super Mesh", hint = "See what is on the whole AREDN mesh" }
     end
-elseif uci.cursor():get("aredn", "@supernode[0]", "enable") == "1" then
-    return { href = "/cgi-bin/mesh", display = "Super Mesh", hint = "See what is on the supernode mesh" }
 end
