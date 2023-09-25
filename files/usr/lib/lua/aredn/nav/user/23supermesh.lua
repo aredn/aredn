a2695217aad8859c53c3dfee335ed7b02d8e1e83
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2023-09-24 18:23:55 -0700

    Move Super Mesh button onto mesh page

--- files/usr/lib/lua/aredn/nav/user/23supermesh.lua
+++
@@ -1,6 +0,0 @@
-if nixio.fs.stat("/tmp/dnsmasq.d/supernode.conf") then
-    local ip = read_all("/tmp/dnsmasq.d/supernode.conf"):match("^#(%S+)")
-    if ip then
-        return { href = "http://" .. ip .. "/cgi-bin/mesh", display = "Super Mesh", hint = "See what is on the whole AREDN mesh" }
-    end
-end
