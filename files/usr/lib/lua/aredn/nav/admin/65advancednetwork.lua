cf239fa563dde24f39037bd6cf93ef0aad002ddb
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2024-02-05 10:57:12 -0800

    Collapse QEMU and VMWARE hardware into two basic types

--- files/usr/lib/lua/aredn/nav/admin/65advancednetwork.lua
+++ files/usr/lib/lua/aredn/nav/admin/65advancednetwork.lua
@@ -1,4 +1,4 @@
 local board = aredn.hardware.get_board_type()
-if board == "mikrotik,hap-ac2" or board == "mikrotik,hap-ac3" or board == "qemu-standard-pc-i440fx-piix-1996" or board == "glinet,gl-b1300" or board == "VMware, Inc. VMware Virtual Platform" then
+if board == "mikrotik,hap-ac2" or board == "mikrotik,hap-ac3" or board == "qemu" or board == "glinet,gl-b1300" or board == "vmware" then
     return { href = "advancednetwork", display = "Advanced Network" }
 end
