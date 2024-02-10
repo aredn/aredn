139c5f66e58e641d2b21fabd5a2eca84fba87566
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2024-02-09 20:45:52 -0800

    Merge the LQM info onto the Mesh page (#1095)

--- files/usr/lib/lua/aredn/nav/user/20lqm.lua
+++
@@ -1,3 +0,0 @@
-if uci.cursor():get("aredn", "@lqm[0]", "enable") == "1" then
-  return { href = "lqm", display = "Neighbor Status", hint = "See the link status to our neighbors", enable = not config_mode }
-end
