6d81b4c84633d6647720a32297c77c51e3a38238
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2024-02-28 23:57:13 -0800

    Enable xlinks on hap ac lites (#1104)

--- files/usr/lib/lua/aredn/nav/admin/65advancednetwork.lua
+++ files/usr/lib/lua/aredn/nav/admin/65advancednetwork.lua
@@ -1,4 +1,4 @@
 local board = aredn.hardware.get_board_type()
-if board == "mikrotik,hap-ac2" or board == "mikrotik,hap-ac3" or board == "qemu" or board == "glinet,gl-b1300" or board == "vmware" then
+if board == "mikrotik,routerboard-952ui-5ac2nd" or board == "mikrotik,hap-ac2" or board == "mikrotik,hap-ac3" or board == "qemu" or board == "glinet,gl-b1300" or board == "vmware" then
     return { href = "advancednetwork", display = "Advanced Network" }
 end
